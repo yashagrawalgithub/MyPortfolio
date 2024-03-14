19f123f4adfadec7a0ddcab9ed6da45d6860a948
Yash Agrawal <63327086+yashagrawalgithub@users.noreply.github.com>
2024-03-14 14:17:40 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,34 @@
+// Typewriter effect
+const typedTextSpan = document.getElementById('typed');
+const textArray = ["Full Stack Developer !"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
